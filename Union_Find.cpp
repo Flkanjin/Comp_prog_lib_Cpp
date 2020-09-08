9bb008f717cb5f23d0f29988c4d5d08420d1b706
Flkanjin <kan.shota64@gmail.com>
2020-09-08 15:50:23 +0900

    Update Union_Find.cpp
    AtCoder Libraryのようにメンバ変数を減らす。
    関数groups()の追加
    TをAbelianGに

--- Union_Find.cpp
+++ Union_Find.cpp
@@ -1,31 +1,26 @@
+#include <algorithm>
 #include <vector>
 
 
 class UnionFind{
-    std::vector<int> par;
-    std::vector<int> rank;
-    std::vector<int> membnum;
+    int n;
+    std::vector<int> par;//根ならマイナス*要素数
 
 public:
-    UnionFind(){}
-    UnionFind(int n): par(n), rank(n), membnum(n){
+    UnionFind(): n(0){}
+    UnionFind(int N): n(N), par(n){
         for(int i(0); i < n; ++i){
-            par[i] = i;
-            rank[i] = 0;
-            membnum[i] = 1;
+            par[i] = -1;
         }
     }
 
-    void resize(int n){
-        int m(par.size());
-        par.resize(n);
-        rank.resize(n, 0);
-        membnum.resize(n, 1);
-        for(int i(m); i < n; ++i) par[i] = i;
+    void resize(int N){
+        n = N;
+        par.resize(n, -1);
     }
 
     int find(int x){
-        if(x == par[x]) return x;
+        if(par[x] < 0) return x;
         else return par[x] = find(par[x]);
     }
 
@@ -34,14 +29,9 @@ public:
         y = find(y);
         if(x == y) return false;
 
-        if(rank[x] < rank[y]){
-            par[x] = y;
-            membnum[y] += membnum[x];
-        }else{
-            par[y] = x;
-            if(rank[x] == rank[y]) ++rank[x];
-            membnum[x] += membnum[y];
-        }
+        if(-par[x] < -par[y]) std::swap(x, y); 
+        par[x] += par[y];
+        par[y] = x;
         return true;
     }
 
@@ -50,65 +40,77 @@ public:
     }
 
     int howmany(int x){ //同じグループに属すものの個数(自分を含む)
-        return membnum[find(x)];
+        return -par[find(x)];
+    }
+
+    std::vector<std::vector<int>> groups(){
+        std::vector<int> root_buf(n), group_size(n);
+        for(int i(0); i < n; ++i){
+            root_buf[i] = find(i);
+            ++group_size[root_buf[i]];
+        }
+        std::vector<std::vector<int>> ret(n);
+        for(int i(0); i < n; ++i){
+            ret[i].reserve(group_size[i]);
+        }
+        for(int i(0); i < n; ++i){
+            ret[root_buf[i]].push_back(i);
+        }
+        ret.erase(
+            std::remove_if(std::begin(ret), std::end(ret),
+                [&](const std::vector<int>& v){return v.empty();}),
+            std::end(ret));
+        return ret;
     }
 };
 
-//Tはアーベル群
-template<class T> class WeightedUnionFind{
-    std::vector<int> par;
-    std::vector<int> rank;
-    std::vector<int> membnum;
-    std::vector<T> diff_weight;
+//AbelianGはアーベル群
+template<class AbelianG> class WeightedUnionFind{
+    int n;
+    std::vector<int> par;//根ならマイナス*要素数
+    std::vector<AbelianG> diff_weight;
  
 public:
-    WeightedUnionFind(){}
-    WeightedUnionFind(int n, T UNITY = 0): par(n), rank(n), membnum(n), diff_weight(n){
-        for(int i(0); i < n; ++i){
-            par[i] = i;
-            rank[i] = 0;
-            membnum[i] = 1;
-            diff_weight[i] = UNITY;
+    WeightedUnionFind(): n(0){}
+    WeightedUnionFind(int N, AbelianG UNITY = 0):
+        n(N), par(n), diff_weight(n){
+            for(int i(0); i < n; ++i){
+                par[i] = -1;
+                diff_weight[i] = UNITY;
+            }
         }
-    }
  
-    void  resize(int n, T UNITY = 0){
-        int m(par.size());
-        par.resize(n);
-        rank.resize(n, 0);
-        membnum.resize(n, 1);
+    void resize(int N, AbelianG UNITY = 0){
+        n = N;
+        par.resize(n, -1);
         diff_weight.resize(n, UNITY);
-        for(int i(m); i < n; ++i) par[i] = i;
     }
  
     int find(int x){
-        if(x == par[x]) return x;
+        if(par[x] < 0) return x;
         int r(find(par[x]));
         diff_weight[x] += diff_weight[par[x]];
         return par[x] = r;
     }
  
-    T weight(int x){
+    AbelianG weight(int x){
         find(x);
         return diff_weight[x];
     }
  
     //weight(y) = weight(x) + w
-    bool unit(int x, int y, T w){
+    bool unit(int x, int y, AbelianG w){
         w += weight(x); w -= weight(y);
         x = find(x); y = find(y);
         if(x == y) return false;
- 
-        if(rank[x] < rank[y]){
-            par[x] = y;
-            diff_weight[x] = -w;
-            membnum[y] += membnum[x];
-        }else{
-            par[y] = x;
-            if(rank[x] == rank[y]) ++rank[x];
-            diff_weight[y] = w;
-            membnum[x] += membnum[y];
+
+        if(-par[x] < -par[y]){
+            std::swap(x, y); w *= -1;
         }
+
+        par[x] += par[y];
+        par[y] = x;
+        diff_weight[y] = w;
         return true;
     }
  
@@ -117,6 +119,26 @@ public:
     }
  
     int howmany(int x){ //同じグループに属すものの個数(自分を含む)
-        return membnum[find(x)];
+        return -par[find(x)];
+    }
+
+    std::vector<std::vector<int>> groups(){
+        std::vector<int> root_buf(n), group_size(n);
+        for(int i(0); i < n; ++i){
+            root_buf[i] = find(i);
+            ++group_size[root_buf[i]];
+        }
+        std::vector<std::vector<int>> ret(n);
+        for(int i(0); i < n; ++i){
+            ret[i].reserve(group_size[i]);
+        }
+        for(int i(0); i < n; ++i){
+            ret[root_buf[i]].push_back(i);
+        }
+        ret.erase(
+            std::remove_if(std::begin(ret), std::end(ret),
+                [&](const std::vector<int>& v){return v.empty();}),
+            std::end(ret));
+        return ret;
     }
 };
