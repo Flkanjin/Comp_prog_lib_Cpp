dfa742005a18fc0775cc374e6dedba28b70bad20
Flkanjin <kan.shota64@gmail.com>
2021-01-09 00:32:22 +0900

    Update 点,ベクトル,線2D.cpp
    double -> long double
    外積を修正
    点の回転を追加
    交点を追加
    円、多角形の追加

--- 点,ベクトル,線2D.cpp
+++ 点,ベクトル,線2D.cpp
@@ -1,3 +1,4 @@
+#include <algorithm>
 #include <cassert>
 #include <cmath>
 #include <utility>
@@ -6,9 +7,9 @@ const long double EPS = 1E-10;
 
 
 class Point2D{
-    double x, y;
+    long double x, y;
 public:
-    Point2D(double X=0, double Y=0): x(X), y(Y){}
+    Point2D(long double X=0, long double Y=0): x(X), y(Y){}
     Point2D(const Point2D& v): x(v.x), y(v.y){}
     template<class T>
     Point2D(const std::vector<T> &v){
@@ -20,10 +21,10 @@ public:
         x = p.first; y = p.second;
     }
 
-    void setX(double X){x = X;}
-    double getX(){return x;}
-    void setY(double Y){y = Y;}
-    double getY(){return y;}
+    void setX(long double X){x = X;}
+    long double getX(){return x;}
+    void setY(long double Y){y = Y;}
+    long double getY(){return y;}
 
     Point2D operator+() const{return *this;}
     Point2D operator-() const{return Point2D(-x, -y);}
@@ -43,12 +44,12 @@ public:
         return *this;
     }
 
-    Point2D& operator*=(const double a){
+    Point2D& operator*=(const long double a){
         x *= a; y *= a;
         return *this;
     }
 
-    Point2D& operator/=(const double a){
+    Point2D& operator/=(const long double a){
         x /= a; y /= a;
         return *this;
     }
@@ -63,24 +64,24 @@ public:
         return tmp -= v;
     }
 
-    Point2D operator*(const double a) const{
+    Point2D operator*(const long double a) const{
         Point2D tmp(*this);
         return tmp *= a;
     }
 
-    friend Point2D operator*(double a, const Point2D v){
+    friend Point2D operator*(long double a, const Point2D v){
         return v * a;
     }
 
-    Point2D operator/(const double a) const{
+    Point2D operator/(const long double a) const{
         Point2D tmp(*this);
         return tmp /= a;
     }
 
-    double norm(){return x*x + y*y;}
-    friend double norm(Point2D v){return v.norm();}
-    double abs(){return sqrt(norm());}
-    friend double abs(Point2D v){return v.abs();}
+    long double norm(){return x*x + y*y;}
+    friend long double norm(Point2D v){return v.norm();}
+    long double abs(){return sqrt(norm());}
+    friend long double abs(Point2D v){return v.abs();}
 
     bool operator<(const Point2D &v) const{
         return x != v.x ? x < v.x : y < v.y;
@@ -103,23 +104,23 @@ public:
         return (*this > v) || (*this == v);
     }
 
-    double dot(Point2D b){
+    long double dot(Point2D b){
         return x*b.x + y*b.y;
     }
 
-    friend double dot(Point2D a, Point2D b){
+    friend long double dot(Point2D a, Point2D b){
         return a.dot(b);
     }
 
-    double cross(Point2D b){
-        return x*b.x - y*b.x;
+    long double cross(Point2D b){
+        return x*b.y - y*b.x;
     }
 
-    friend double cross(Point2D a, Point2D b){
+    friend long double cross(Point2D a, Point2D b){
         return a.cross(b);
     }
 
-    friend double distance(Point2D a, Point2D b){
+    friend long double distance(Point2D a, Point2D b){
         return (a - b).abs();
     }
 
@@ -155,6 +156,16 @@ public:
         return(p1.ccw(p2, p3) * p1.ccw(p2, p4) <= 0 &&
                p3.ccw(p4, p1) * p3.ccw(p4, p2) <= 0);
     }
+
+    Point2D rotateDegree(long double ang){
+        long double rad(ang * M_PI / 180);
+        long double cs(std::cos(rad)), sn(std::sin(rad));
+        return Point2D(x * cs - y * sn, x * sn + y * cs);
+    }
+
+    Point2D rotateDegree(long double ang, Point2D center){
+        return center + (*this - center).rotateDegree(ang);
+    }
 };
 using Vector2D = Point2D;
 
@@ -171,7 +182,7 @@ public:
 
     Point2D project(Point2D p){
         Point2D base(p2 - p1);
-        double r(dot(p - p1, base) / norm(base));
+        long double r(dot(p - p1, base) / norm(base));
         return p1 + base * r;
     }
 
@@ -194,5 +205,48 @@ public:
     friend bool intersect(Line2D s1, Line2D s2){
         return intersect(s1.p1, s1.p2, s2.p1, s2.p2);
     }
+
+    friend Point2D getCrossPoint(Line2D s1, Line2D s2){
+        Vector2D base(s2.p2 - s2.p1);
+        long double d1(std::abs(cross(base, s1.p1 - s2.p1)));
+        long double d2(std::abs(cross(base, s1.p2 - s2.p1)));
+        long double t(d1 / (d1 + d2));
+        return s1.p1 + (s1.p2 - s1.p1) * t;
+    }
 };
 using Segment2D = Line2D;
+
+class Circle2D{
+    Point2D center;
+    long double radius;
+public:
+    Circle2D(Point2D c = Point2D(0, 0), long double r = 0.0): center(c), radius(r){}
+    Circle2D(const Circle2D& cir): center(cir.center), radius(cir.radius){}
+
+    void setCenter(Point2D c){center = c;}
+    Point2D getCenter(){return center;}
+    void setRadius(long double r){radius = r;}
+    long double getRadius(){return radius;}
+};
+
+using Polygon2D = std::vector<Point2D>;
+
+Polygon2D convex_hll(std::vector<Point2D> &v){
+    int n(v.size()), k(0);
+    auto comp = [](Point2D a, Point2D b){
+        if(a.getX() != b.getX()) return a.getX() < b.getX();
+        else return a.getY() < b.getY();
+    };
+    std::sort(std::begin(v), std::end(v), comp);
+    std::vector<Point2D> ret(2*n);
+    //下側
+    for(int i(0); i < n; ret[k++] = v[i++]){
+        while(k >= 2 && ret[k-2].ccw(ret[k-1], v[i]) <= 0) --k;
+    }
+    //上側
+    for(int i(n-2), t = k + 1; i >= 0; ret[k++] = v[i--]){
+        while(k >= t && ret[k-2].ccw(ret[k-1], v[i]) <= 0) --k;
+    }
+    ret.resize(k-1);
+    return ret;
+}
