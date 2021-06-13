5a770ebe1505e1aae202d63a1c4bc5de39bffd5e
Flkanjin <kan.shota64@gmail.com>
2021-06-13 23:44:01 +0900

    Update Comb_perm.cpp
    () -> {}
    resize

--- Comb_perm.cpp
+++ Comb_perm.cpp
@@ -9,10 +9,10 @@ public:
     comb_perm(): n(0), fact(1), fact_inv(1){
         fact[0] = fact_inv[0] = 1;
     }
-    comb_perm(int N):n(N), fact(N+1), fact_inv(N+1){
+    comb_perm(int N): n(N), fact(N+1), fact_inv(N+1){
         fact[0] = fact[1] = fact_inv[0] = fact_inv[1] = 1;
-        for(int i(2); i <= n; ++i) fact[i] = fact[i - 1] * i % m;
-        for(int i(2); i <= n; ++i) fact_inv[i] = inv(fact[i]);
+        for(int i{2}; i <= n; ++i) fact[i] = fact[i - 1] * i % m;
+        for(int i{2}; i <= n; ++i) fact_inv[i] = inv(fact[i]);
     }
  
     bool resize(int N){
@@ -22,14 +22,14 @@ public:
         if(!n && N){
             fact[1] = fact_inv[1] = 1;
         }
-        for(int i(std::max(2, n+1)); i <= N; ++i) fact[i] = fact[i - 1] * i % m;
-        for(int i(std::max(2, n+1)); i <= N; ++i) fact_inv[i] = inv(fact[i]);
+        for(int i{std::max(2, n+1)}; i <= N; ++i) fact[i] = fact[i - 1] * i % m;
+        for(int i{std::max(2, n+1)}; i <= N; ++i) fact_inv[i] = inv(fact[i]);
         n = N;
         return true;
     }
  
     long long power(long long a, unsigned long long b){
-        long long ret(1);
+        long long ret{1};
         while(b){
             if(b & 1) ret = (ret * a) % m;
             a = (a * a) % m;
@@ -41,22 +41,22 @@ public:
     }
  
     long long inv(long long a){ // GCD(a, m) = 1
-        return power(a, m-2);
+        return power(a, m - 2);
     }
  
     long long permutation(int n, int r){
         if(n < r) return 0;
         if(n < 0 || r < 0) return 0;
-        long long ret(fact[n]);
-        ret = ret * fact_inv[n - r] % m;
+        long long ret{factorial(n)};
+        ret = ret * inv_factorial(n - r) % m;
         return ret;
     }
  
     long long combination(int n, int r){
         if(n < r) return 0;
         if(n < 0 || r < 0) return 0;
-        long long ret(permutation(n, r));
-        return ret * fact_inv[r] % m;
+        long long ret{permutation(n, r)};
+        return ret * inv_factorial(r) % m;
     }
 
     long long multi_combination(int n, int r){
@@ -64,10 +64,12 @@ public:
     }
  
     long long factorial(int a){
+        if(a > n) resize(a);
         return fact[a];
     }
  
     long long inv_factorial(int a){
+        if(a > n) resize(a);
         return fact_inv[a];
     }
 
